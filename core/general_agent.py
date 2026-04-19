5b3e248b9f888613e22c205093dafbe04984f5f8
Ian Farai Madhara <iannyfarai@gmail.com>
2026-04-18 23:55:57 -0400

    added general agent

---
+++ core/general_agent.py
@@ -0,0 +1,63 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+def prompt_open_ai():
+    client = OpenAI()
+
+# Few shot prompting: Directly giving instructions to the mddel and few examples to the model
+    SYSTEM_PROMPT = """
+You're an expert AI Assistant in resolving user queries using chain of thought
+You work on START, PLAN and OUTPUT steps.
+You need to first PLAN what needs to be done. The PLAN can be multiple steps.
+Once you think enough PLAN has been done, finally you can give an OUTPUT
+
+Rule:
+- Strictly follow the given JSON output format
+- Only run one step at a time
+- The sequence of steps is START (where user gives an input), PLAN 
+(That can be multiple times) and finally OUTPUT (which is going to be displayed to the user)
+
+Output JSON Format:
+{"step": "START" | "PLAN" | "OUTPUT", "content":"string"}
+
+
+Examples:
+START: Can you solve 2 + 3 * 5 / 10
+PLAN {"STEPS":"PLAN", "content": "Seems like user is interested in math problem"}
+PLAN {"STEPS":"PLAN", "content": "looking at the problem, we should solve this using BODMAS method"}
+PLAN {"STEPS":"PLAN", "content": "Yes, BODMAS is correct thing to be done here"}
+PLAN {"STEPS":"PLAN", "content": "first we must multiply 3 * 5 which is 15"}
+PLAN {"STEPS":"PLAN", "content": "Now the new equation is 2 + 15 /  10"}
+PLAN {"STEPS":"PLAN", "content": "We must perform division that is 15 / 10 = 1.5"}
+PLAN {"STEPS":"PLAN", "content": "Now the new equation is 2 + 1.5"}
+
+
+
+
+
+
+
+Q. Hey write a code in python for adding two numbers.
+A: {{'code': 'def add(a:int, b:int)->int:
+        return a + a','isCodingQuestion':true }}
+"""
+
+    response = client.responses.create(
+        model="gpt-5.4",
+        input=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": "implemement factorial function in python"
+            }
+        ]
+    )
+    print(response)
+    print(response.output_text)
+
+if __name__ == "__main__":
+    prompt_open_ai()
