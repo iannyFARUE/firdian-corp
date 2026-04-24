0668ca09f3461984e7a4c042b2b6d81aa995fd19
Ian Farai Madhara <iannyfarai@gmail.com>
2026-04-23 22:55:38 -0400

    added integration with ollama

--- server/main.py
+++ server/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from ollama import Client
 
 app = FastAPI()
+client = Client(
+    host="http://localhost:11434"
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/chat")
+def chat(message: str = Body(..., description="The message")):
+    response = client.chat(model="gemma:2b", messages=[
+        {"role":"user","content":message}
+    ])
+    return {"response":response.message.content}
