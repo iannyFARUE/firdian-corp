4029e5f75d3280237ba6f03828d5db358d2428bc
Ian Farai Madhara <iannyfarai@gmail.com>
2026-04-27 11:57:00 -0400

    added route to post query to queue

--- server/main.py
+++ server/main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI, Body
+from dotenv import load_dotenv
+load_dotenv()
+from fastapi import FastAPI, Body, Query
 from ollama import Client
+from rag.queues.worker import  process_query
+from rag.client.rq_client import get_queue
+
 
 app = FastAPI()
 client = Client(
@@ -12,9 +17,14 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/chat")
-def chat(message: str = Body(..., description="The message")):
-    response = client.chat(model="gemma2:2b", messages=[
-        {"role":"user","content":message}
-    ])
-    return {"response":response.message.content}
+# @app.post("/chat")
+# def chat(message: str = Body(..., description="The message")):
+#     response = client.chat(model="gemma2:2b", messages=[
+#         {"role":"user","content":message}
+#     ])
+#     return {"response":response.message.content}
+
+@app.post('/chat')
+async def chat(query:str =  Query(..., description="The chat query of users")):
+    job = get_queue().enqueue(process_query, query)
+    return {"status":"queued","job_id":job.id}
